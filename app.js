958c6524653f35b927a09222f4dd6a2b5ad6e8b4
YASH-PRAJAPATI-4903 <114235226+YASH-PRAJAPATI-4903@users.noreply.github.com>
2022-10-05 19:12:01 +0530

    yy
    yy

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-
 function getPassword(){
     var chars ="123456abcdABCD*";
-    var passwordlen = 4;
+    var passwordlen = 8;
     var password="";
     for (var i =0; i < passwordlen ;i++) {
         var random = Math.floor(Math.random()*chars.length)
